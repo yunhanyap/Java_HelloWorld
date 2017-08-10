bab394819f5c60bf6620703bd7e342475c621685
yunhanyap <yunhan.yap@alumni.ubc.ca>
2017-08-10 17:54:30 +0800

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 1;
+
+        System.out.println("Hello, World!");
+        System.out.println(x);
+    }
+}
