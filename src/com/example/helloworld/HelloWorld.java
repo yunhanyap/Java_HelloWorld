61e0137ab24c76075039e5e825227c13ee5a74c6
yunhanyap <yunhan.yap@alumni.ubc.ca>
2017-08-10 18:03:12 +0800

    Second Commit into github

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,12 +3,13 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         int x = 0;
-        int count;
-        count = 0;
+        int count = 3;
         
         System.out.println("Hello, World!");
-        System.out.println(x " + " count);
-        count++;
-        
+        while (x < count) {
+            System.out.println(count);
+            x++;
+        }
+        System.out.println(x);
     }
 }
