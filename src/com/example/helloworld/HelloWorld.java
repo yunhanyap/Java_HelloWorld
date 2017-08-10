63f6ef53c6f6a07ed74767eefc41eb195d48d213
Yun Han YAP <yunhanyap@users.noreply.github.com>
2017-08-10 17:57:19 +0800

    Update HelloWorld.java

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -2,9 +2,13 @@ package com.example.helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int x = 1;
-
+        int x = 0;
+        int count;
+        count = 0;
+        
         System.out.println("Hello, World!");
-        System.out.println(x);
+        System.out.println(x " + " count);
+        count++;
+        
     }
 }
