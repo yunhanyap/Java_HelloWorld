b3d75b7d897119d40a684f613d6f425452b13b66
yunhanyap <yunhan.yap@alumni.ubc.ca>
2017-08-10 18:13:08 +0800

    Added comments about project

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,5 +1,10 @@
 package com.example.helloworld;
 
+/* Created by Yun Han Yap Aug 10th, 2017
+*  - Learning how to use GitHub as a working repository for Java Projects
+*  - Initial Project Class as always Hello World
+*/
+
 public class HelloWorld {
     public static void main(String[] args) {
         int x = 0;
